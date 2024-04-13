335f1aea028b7adae9c724719ecf3f79465277c1
Nova840 <adamzmudzinski2@gmail.com>
2024-04-13 06:39:18 -0400

    Make vehicle rotate on slopes

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -23,10 +23,14 @@ public class Vehicle : MonoBehaviour {
     [SerializeField]
     private float springDamper;
 
+    [SerializeField]
+    private float vehicleRotateMaxAngle;
+
     private Rigidbody _rigidbody;
 
     private Vector3 velocityXZ = Vector3.zero;
     private Vector3 targetXZDirection;
+    private Vector3 lastHitNormal = Vector3.up;
 
     private void Awake() {
         _rigidbody = GetComponent<Rigidbody>();
@@ -34,15 +38,20 @@ public class Vehicle : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        bool didHit = GroundRay(out RaycastHit hit);
+
         Vector2 inputVector = new Vector2(Keyboard.current.dKey.value - Keyboard.current.aKey.value, Keyboard.current.wKey.value - Keyboard.current.sKey.value);
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
 
         if (moveDirection != Vector3.zero) {
             targetXZDirection = Vector3.RotateTowards(transform.forward, moveDirection, turningSpeed * Time.deltaTime, 0);
         }
-        transform.rotation = Quaternion.LookRotation(targetXZDirection);
+        transform.rotation = Quaternion.LookRotation(lastHitNormal, targetXZDirection) * Quaternion.Euler(-90, 0, 180);
 
-        AddUpwardForce();
+
+        if (didHit) {
+            AddUpwardForce(hit);
+        }
 
         float speed = moveDirection == Vector3.zero ? 0 : topSpeed;
         velocityXZ = Vector3.Lerp(velocityXZ, transform.forward * speed, acceleration * Time.deltaTime);
@@ -52,19 +61,21 @@ public class Vehicle : MonoBehaviour {
 
     private bool GroundRay(out RaycastHit hit) {
         Debug.DrawRay(transform.position, Vector3.down * rayDistance, Color.red, 0, true);
-        return Physics.Raycast(transform.position, Vector3.down, out hit, rayDistance, ~LayerMask.GetMask("Vehicle"), QueryTriggerInteraction.Ignore);
+        bool didHit = Physics.Raycast(transform.position, Vector3.down, out hit, rayDistance, ~LayerMask.GetMask("Vehicle"), QueryTriggerInteraction.Ignore);
+        if (didHit && Vector3.Angle(hit.normal, Vector3.up) <= vehicleRotateMaxAngle) {
+            lastHitNormal = hit.normal;
+        }
+        return didHit;
     }
 
     //https://youtu.be/CdPYlj5uZeI?si=DAdpflGTnhWeMEBA
-    private void AddUpwardForce() {
-        if (GroundRay(out RaycastHit hit)) {
-            Vector3 springDir = Vector3.up;
-            Vector3 worldVel = _rigidbody.velocity;
-            float offset = rayDistance - hit.distance;
-            float vel = Vector3.Dot(springDir, worldVel);
-            float force = (offset * springStrength) - (vel * springDamper);
-            _rigidbody.AddForce(springDir * force, ForceMode.Acceleration);
-        }
+    private void AddUpwardForce(RaycastHit hit) {
+        Vector3 springDir = Vector3.up;
+        Vector3 worldVel = _rigidbody.velocity;
+        float offset = rayDistance - hit.distance;
+        float vel = Vector3.Dot(springDir, worldVel);
+        float force = (offset * springStrength) - (vel * springDamper);
+        _rigidbody.AddForce(springDir * force, ForceMode.Acceleration);
     }
 
 }
