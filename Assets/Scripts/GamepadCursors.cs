53ba3ec979fc8db6fce3db49c83e6c7e41838926
Nova840 <adamzmudzinski2@gmail.com>
2024-04-13 11:38:55 -0400

    Add start menu cursors

---
+++ Assets/Scripts/GamepadCursors.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class GamepadCursors : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private RectTransform[] cursors;
+
+    private void Awake() {
+        InputSystem.onDeviceChange += OnDeviceChange;
+        cursors = new RectTransform[4];
+        for (int i = 0; i < transform.childCount; i++) {
+            cursors[i] = transform.GetChild(i).GetComponent<RectTransform>();
+        }
+        SetCursorVisibility();
+    }
+
+    private void OnDestroy() {
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    private void Update() {
+        for (int i = 0; i < Gamepad.all.Count; i++) {
+            cursors[i].transform.position += (Vector3)(Gamepad.all[i].leftStick.value * (moveSpeed * Time.deltaTime));
+            cursors[i].transform.position = new Vector3(
+                Mathf.Clamp(cursors[i].transform.position.x, 0, Screen.width),
+                Mathf.Clamp(cursors[i].transform.position.y, 0, Screen.height),
+                0
+            );
+
+            if (Gamepad.all[i].buttonSouth.wasPressedThisFrame) {
+                PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+                pointerEventData.position = cursors[i].transform.position;
+                List<RaycastResult> results = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(pointerEventData, results);
+                Button[] buttons = results.Select(r => r.gameObject.GetComponent<Button>()).Where(b => b).ToArray();
+                if (buttons.Length > 0) {
+                    buttons[0].onClick.Invoke();
+                }
+            }
+        }
+    }
+
+    private void SetCursorVisibility() {
+        for (int i = 0; i < cursors.Length; i++) {
+            cursors[i].gameObject.SetActive(i < Gamepad.all.Count);
+        }
+    }
+
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change) {
+        SetCursorVisibility();
+    }
+
+}
