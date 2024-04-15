a46f578eebe0038cf230493e8276daac29e3d7d5
Nova840 <adamzmudzinski2@gmail.com>
2024-04-15 17:57:34 -0400

    Added bounce pad summon

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour {
+
+    [SerializeField]
+    private float upForce;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Vehicle")) return;
+        Vehicle vehicle = other.GetComponentInParent<Vehicle>();
+        if (vehicle.FinishLineTrigger != other) return;
+        Rigidbody rigidbody = vehicle.GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3.up * upForce, ForceMode.VelocityChange);
+    }
+
+}
